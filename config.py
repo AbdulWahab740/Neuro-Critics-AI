59bf9de69b205a174f79aaf5a316862a6cca9695
Abdul Wahab <148485953+AbdulWahab740@users.noreply.github.com>
2025-08-30 05:59:07 -0700

    Update config.py

--- config.py
+++ config.py
@@ -11,5 +11,6 @@ EMBEDDING_MODEL_NAME = 'all-MiniLM-L6-v2'
 FAISS_INDEX_PATH = "./faiss_index"
 COLLECTION_NAME = "neuro_research_critics"
 # GEMINI_API_KEY = st.secrets("GEMINI_API_KEY")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "your_default_gemini_api_key_here")
+GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
 TOPIC_CLASSIFICATION_THRESHOLD = 0.55
+
